197a16e9fc6dab4c6c33b6d0e4653cd257bee9dc
Laksh Bhambhani <lakshbh@gmail.com>
2019-03-25 09:56:07 -0700

    Demo is SHITASS!!!!

--- Demo.py
+++ Demo.py
@@ -9,56 +9,77 @@ m3 = PiMotor.Motor("MOTOR3",1)
 m4 = PiMotor.Motor("MOTOR4",1)
 
 us=Sensor("ULTRASONIC",30)
-ir1=Sensor("IR1",4)
-ir2=Sensor("IR2",4)
+ir1=Sensor("IR1",10)
+ir2=Sensor("IR2",10)
 
 #To drive all motors together
 motorAll = PiMotor.LinkedMotors(m1,m2,m3,m4)
 
-try:
-    while True:
-        motorAll.reverse(60)
-        time.sleep(0.3)
-        TrigIR()
-        m1.reverse(100)
-        time.sleep(0.1)
-        motorAll.reverse(100)
-        TrigIR()
-        time.sleep(1)
-        motorAll.stop()
-        TrigIR()
-        while True:
-            TrigIR()
-            us.trigger()
-            if us.Triggered:
-                while us.Triggered:
-                    motorAll.forward(100)
-                    TrigIR()
-                motorAll.reverse(100)
-                TrigIR()
-                time.sleep(1)
-                motorAll.stop()
-            else:
-                motorAll.forward(100)
-                TrigIR()
-                time.sleep(1)
-                motorAll.stop()
-                TrigIR()
-                m2.forward(100)
-                TrigIR()
-                us.trigger()
-                time.sleep(0.5)
-                TrigIR()
-                motorAll.stop()
-            
+x=0
+
 def TrigIR():
     ir1.trigger()
     ir2.trigger()
-    if ir1.Triggered or ir2.Triggered:
-        motorAll.reverse(100)
-        time.sleep(2)
+    if ir1.Triggered:
+        motorAll.reverse(70)
+        time.sleep(1)
         motorAll.stop()
+        print("Front Line Detected")
+    if ir2.Triggered:
+        motorAll.forward(70)
+        time.sleep(1)
+        motorAll.stop()
+        print("Back Line Detected")
 
+try:
+    motorAll.reverse(100)
+    TrigIR()
+    time.sleep(1.7)
+    motorAll.stop()
+    TrigIR()
+    m1.forward(50)
+    time.sleep(0.5)
+    motorAll.stop()
+    motorAll.forward(100)
+    TrigIR()
+    time.sleep(1.7)
+    motorAll.stop()
+    TrigIR()
+    while True:
+        us.trigger()
+        TrigIR()
+        if us.Triggered:
+            for x in range(10):
+                us.trigger()
+                TrigIR()
+                if us.Triggered:
+                    motorAll.forward(100)
+                    TrigIR()
+            motorAll.reverse(60)
+            TrigIR()
+            time.sleep(1)
+            motorAll.stop()
+            TrigIR()
+        else:
+            m1.forward(50)
+            m2.reverse(50)
+            TrigIR()
+            time.sleep(5)
+            motorAll.stop()
+            x=x+1
+            if x==1:
+                motorAll.forward(40)
+                TrigIR()
+                time.sleep(1)
+                TrigIR()
+                motorAll.stop()
+            TrigIR()
+        
 
 except KeyboardInterrupt:
     GPIO.cleanup()
+            
+
+
+
+
