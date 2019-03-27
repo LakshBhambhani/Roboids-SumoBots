a00519b021d519e9bd44cb2ce6b6fb7ca32d9d5b
Laksh Bhambhani <lakshbh@gmail.com>
2019-03-26 18:59:19 -0700

    Update Demo.puy

--- Demo.py
+++ Demo.py
@@ -9,8 +9,8 @@ m3 = PiMotor.Motor("MOTOR3",1)
 m4 = PiMotor.Motor("MOTOR4",1)
 
 us=Sensor("ULTRASONIC",30)
-ir1=Sensor("IR1",10)
-ir2=Sensor("IR2",10)
+ir1=Sensor("IR2",10)
+ir2=Sensor("IR1",10)
 
 #To drive all motors together
 motorAll = PiMotor.LinkedMotors(m1,m2,m3,m4)
@@ -21,23 +21,27 @@ def trigIR():
     ir1.trigger()
     ir2.trigger()
     if ir1.Triggered:
-        motorAll.reverse(70)
-        time.sleep(1)
+        motorAll.reverse(100)
+        time.sleep(0.7)
         motorAll.stop()
         print("Front Line Detected")
     if ir2.Triggered:
-        motorAll.forward(70)
-        time.sleep(1)
+        motorAll.forward(100)
+        time.sleep(0.7)
         motorAll.stop()
         print("Back Line Detected")
 
 try:
-    motorAll.reverse(100)
-    trigIR()
-    time.sleep(1.7)
-    trigIR()
-    motorAll.stop()
-    trigIR()
+    print ("Going to the left...")
+    m1.reverse(75)
+    m2.reverse(90)
+    time.sleep(1.9)
+    motorAll.stop()   
+    print ("Going to the right...")
+    m1.forward(100)
+    m2.forward(60)
+    time.sleep(1.8)
+    motorAll.stop() 
     while True:
         us.trigger()
         trigIR()
@@ -67,9 +71,9 @@ try:
                     if us.Triggered:
                         motorAll.forward(100)
                         trigIR()
-            time.sleep(0.3)
-            motorAll.stop()
+            time.sleep(0.2)
             trigIR()
+            motorAll.stop()
             x=x+1
             print(x)
             if x%3==0:
@@ -84,8 +88,3 @@ try:
 
 except KeyboardInterrupt:
     GPIO.cleanup()
-            
-
-
-
-
