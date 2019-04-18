533a97aa17391d38d6e20222e66f653b4fceeaec
Laksh Bhambhani <lakshbh@gmail.com>
2019-04-18 15:40:49 -0700

    Update Demo5.py

--- Demo5.py
+++ Demo5.py
@@ -16,8 +16,6 @@ ar = PiMotor.Arrow(4)
 us=Sensor("ULTRASONIC",120)
 irFront=Sensor("IR1",1)
 irBack=Sensor("IR2",1)
-irLeft=Sensor("IR3",1)
-irRight=Sensor("IR4",1)
 
 #To drive all motors together
 motorAll = PiMotor.LinkedMotors(m1,m2,m3,m4)
@@ -29,30 +27,28 @@ def trigIR():
     irBack.trigger()
     if irFront.Triggered:
         motorAll.reverse(100)
-        time.sleep(1.8)
+        time.sleep(1)
         motorAll.stop()
         us.boundary = 120
-        print("LA: Front Line Detected")
+        print("Front Line Detected")
     if irBack.Triggered:
         motorAll.forward(100)
-        time.sleep(1.8)
+        time.sleep(1)
         motorAll.stop()
         us.boundary = 120
-        print("LA: Back Line Detected")
+        print("Back Line Detected")
 
 def findOpponent():
-    print("LA: Finding")
+    print("Finding")
     trigIR()
     while True:
-	print("Last Read:")
-	print(us.lastRead)
         trigIR()
         us.boundary = 122
         us.trigger()
         time.sleep(0.001)
         motorAll.stop()
         if us.Triggered:
-            print("LA: Found")
+            print("Found")
             trigIR()
             break
         leftMotors.forward(75)
@@ -68,14 +64,21 @@ def findOpponent():
 	        print("count:")
 	        print(count)
                 if irFront.Triggered:
-	    	    count = count + 1
+	    	        count = count + 1
 	        us.trigger()
 	        trigIR()
-                if us.lastRead <= 3 and not irFront.Triggered and not irBack.Triggered:
+                if us.lastRead <= 3 and not irFront.Triggered:
                     motorSpeed = 100
                 else:
                     motorSpeed = 65
                 motorAll.forward(motorSpeed)
+
+    if count == 2:
+        rightMotors.reverse(100)
+        leftMotors.forward(100)
+        time.sleep(0.8)
+        motorAll.stop()
+        count = 0
         
 
 try:
@@ -83,7 +86,7 @@ try:
     ar.on()
     ab.on()
     af.on()
-    print ("LA: Going to the center...")
+    print ("Going to the center...")
     m1.reverse(70)
     m2.reverse(70)
     m3.reverse(100)
@@ -95,4 +98,3 @@ try:
 
 except KeyboardInterrupt:
     motorAll.stop() 
-    GPIO.cleanup()
